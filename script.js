7370e451b5da079e8390be08b12b11b7426efe2c
Santhosh Donthu <donthusanthoshkumar@gmail.com>
2026-06-15 00:13:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const counter = document.getElementById("counter");
+const incrementBtn = document.getElementById("incrementBtn");
+
+incrementBtn.addEventListener("click", function () {
+  const currentValue = parseInt(counter.innerText);
+  alert(currentValue);
+  counter.innerText = currentValue + 1;
+});
